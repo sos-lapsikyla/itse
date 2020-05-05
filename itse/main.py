b3f73b5cf58ea6cdfe69d2108b45478fb648493d
Pyry Heiskanen <pyry.heiskanen@gmail.com>
2020-05-05 13:59:37 +0300

    Blacken files

--- itse/main.py
+++ itse/main.py
@@ -6,17 +6,17 @@ import uvicorn
 
 import routes
 
+
 async def login_page(request):
-    return JSONResponse({'hello': 'world'})
+    return JSONResponse({"hello": "world"})
+
 
 def create_app():
-    app = Starlette(debug=True, routes=[
-        Route('/auth', routes.Auth.get),
-    ])
+    app = Starlette(debug=True, routes=[Route("/auth", routes.Auth.get),])
     return app
 
 
 app = create_app()
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- itse/routes/auth.py
+++ itse/routes/auth.py
@@ -3,12 +3,13 @@ import os
 from starlette.templating import Jinja2Templates
 
 dirname = os.path.dirname(__file__)
-template_dir = os.path.join(dirname, 'templates')
+template_dir = os.path.join(dirname, "templates")
 templates = Jinja2Templates(directory=template_dir)
 
-class Auth():
+
+class Auth:
     def get(request):
-        return templates.TemplateResponse('login.html', { 'request': request })
+        return templates.TemplateResponse("login.html", {"request": request})
 
     def post(request):
         pass
